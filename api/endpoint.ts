45aec2679eaaa85229ccb55ec0698ca49ed263ed
SON-uit <sonnt.i8301@gmail.com>
2022-12-28 23:00:18 +0700

    edit endpoint api

--- api/endpoint.ts
+++ api/endpoint.ts
@@ -1,4 +1,4 @@
-const mainApi = `http://localhost:3001`;
-// const mainApi = process.env.NEXT_PUBLIC_API as string;
+// const mainApi = `http://localhost:3001`;
+const mainApi = process.env.NEXT_PUBLIC_API as string;
 // // const mainApi = "https://cb4b-2001-ee0-520b-40e0-99a4-c832-2133-e0ca.ap.ngrok.io";
 export { mainApi };
